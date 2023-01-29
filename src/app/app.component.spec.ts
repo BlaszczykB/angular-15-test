57400d7ccfdb18e6c3db24917fde16e39335d346
Your Name <bartlomiej.blaszczik@gmail.com>
2023-01-29 17:21:11 +0100

    test fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -32,8 +32,8 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('ver-15 app is running!');
   });
-  it('should fail', () => {
+  // it('should fail', () => {
 
-    expect(true).toBe(false);
-  });
+  //   expect(true).toBe(false);
+  // });
 });
