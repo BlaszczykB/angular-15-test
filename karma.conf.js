c64bfbd1fe29f8fbf9733041cb02e681dc01d3a2
Your Name <bartlomiej.blaszczik@gmail.com>
2023-02-04 23:42:52 +0100

    junit-reporter

---
+++ karma.conf.js
@@ -0,0 +1,55 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/0.13/config/configuration-file.html
+
+module.exports = function (config) {
+    config.set({
+      basePath: '',
+      frameworks: ['jasmine', 'angular-cli'],
+      plugins: [
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('karma-remap-istanbul'),
+        require('karma-xml-reporter'),
+        require('karma-junit-reporter'),
+        require('angular-cli/plugins/karma')
+      ],
+      files: [
+        { pattern: './src/test.ts', watched: false }
+      ],
+      preprocessors: {
+        './src/test.ts': ['angular-cli']
+      },
+      mime: {
+        'text/x-typescript': ['ts','tsx']
+      },
+      remapIstanbulReporter: {
+        reports: {
+          html: 'coverage',
+          lcovonly: './coverage/coverage.lcov'
+        }
+      },
+      angularCli: {
+        config: './angular-cli.json',
+        environment: 'dev'
+      },
+      reporters: config.angularCli && config.angularCli.codeCoverage
+                ? ['progress', 'karma-remap-istanbul']
+                : ['progress'],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      browsers: ['Chrome'],
+      singleRun: false,
+      junitReporter: {
+        outputDir: 'test', // results will be saved as $outputDir/$browserName.xml
+        outputFile: 'junit.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+        //suite: '', // suite will become the package name attribute in xml testsuite element
+        //useBrowserName: false, // add browser name to report and classes names
+        //nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+        //classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+        //properties: {}, // key value pair of properties to add to the <properties> section of the report
+        //xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+      }
+    });
+  };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { concat, forkJoin, map, Observable, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ver-15';
+
+  /**
+   *
+   */
+  constructor(private http: HttpClient) { }
+
+
+  getAll<T>(url: string, size: number = 500, filters?: any): Observable<T[]> {
+    return this.getAllpriv(url, size, filters ?? {}, 0, [])
+  }
+
+  private getAllpriv<T>(url: string, size: number = 500, filters?: any, page: number = 0, content: T[] = []): Observable<T[]> {
+
+    const params = new HttpParams()
+    return this.http.get(url, { params }).pipe(
+      switchMap((res: any) => {
+        if (res.page.number <= res.page.totalPages) {
+          return this.getAllpriv(url, size, filters, res.page.number, res.content)
+        } else {
+          return concat(of(content, res.content))
+        }
+      }))
+  }
+
+
+  private getAllParallel<T>(url: string, size: number = 500, filters?: any, page: number = 0, content: T[] = []): Observable<T[]> {
+
+    const params = new HttpParams()
+    return this.http.get(url, { params }).pipe(
+      switchMap((res: any) => {
+        if (res.page.number <= res.page.totalPages) {
+          const requests$: Observable<any>[] = []
+
+          for (let i = res.page.number; i < res.page.totalPages; i++) {
+            requests$.push(this.http.get(url))
+          }
+
+          return concat(res.content, forkJoin(requests$).pipe(map((res: any) => res.content)))
+          
+        } else {
+          return concat(of(content, res.content))
+        }
+      }))
+  }
 }
