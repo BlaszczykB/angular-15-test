60272277a31d9785db7c06ceac63ba1977719f53
Your Name <bartlomiej.blaszczik@gmail.com>
2023-02-12 20:55:47 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { SecondaryPageComponent } from './secondary-page/secondary-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: MainPageComponent,
+    path: 'main-page',
+  },
+  {
+    component: SecondaryPageComponent,
+    path: 'secondary-page',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
